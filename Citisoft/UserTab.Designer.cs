449b66ce187cc14038e537342a0673779efabff9
kevinCuni2004 <kevin.cuni2004@gmail.com>
2023-11-17 14:24:13 +0000

    # Added UserTab.cs

---
+++ Citisoft/UserTab.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Citisoft
+{
+    partial class UserTab
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Back_Button = new System.Windows.Forms.Button();
+            this.Help_Button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Back_Button
+            // 
+            this.Back_Button.Location = new System.Drawing.Point(54, 37);
+            this.Back_Button.Name = "Back_Button";
+            this.Back_Button.Size = new System.Drawing.Size(83, 37);
+            this.Back_Button.TabIndex = 0;
+            this.Back_Button.Text = "Back";
+            this.Back_Button.UseVisualStyleBackColor = true;
+            this.Back_Button.Click += new System.EventHandler(this.Back_Button_Click);
+            // 
+            // Help_Button
+            // 
+            this.Help_Button.Location = new System.Drawing.Point(651, 37);
+            this.Help_Button.Name = "Help_Button";
+            this.Help_Button.Size = new System.Drawing.Size(83, 37);
+            this.Help_Button.TabIndex = 1;
+            this.Help_Button.Text = "Help";
+            this.Help_Button.UseVisualStyleBackColor = true;
+            this.Help_Button.Click += new System.EventHandler(this.Help_Button_Click);
+            // 
+            // UserTab
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Help_Button);
+            this.Controls.Add(this.Back_Button);
+            this.Name = "UserTab";
+            this.Text = "UserTab";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Back_Button;
+        private System.Windows.Forms.Button Help_Button;
+    }
+}
---
+++ Citisoft/UserTab.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Citisoft
+{
+    public partial class UserTab : Form
+    {
+        public UserTab()
+        {
+            InitializeComponent();
+        }
+
+        private void Help_Button_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Back_Button_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
