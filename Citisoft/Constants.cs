f3b22c64537b68cd580bccb19c42c74a20028e82
kevinCuni2004 <kevin.cuni2004@gmail.com>
2023-11-17 13:55:33 +0000

    #added constants for profile table

--- Citisoft/Constants.cs
+++ Citisoft/Constants.cs
@@ -9,5 +9,8 @@ namespace Citisoft
     class Constants
     {
         public static string SELECT_PROFILE = "SELECT * FROM Profile WHERE [e-mail]=@e-mail;";
+        public static string ADD_PROFILE = "INSERT INTO Profile ([e-mail], [password]) VALUES (@e-mail,@password);";
+        public static string ADD_PROFILE_DETAILS = "UPDATE Profile SET [admin_id]=@adminID,[username]=@username,[first_name]=@fname,[last_name]=@lname,[age]=@age,[access]=@access,[details]=@details WHERE [e-mail]=@email;";
+
     }
 }
