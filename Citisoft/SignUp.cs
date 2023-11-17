f00c2cfe91b3e96e1a1268e3f1b5ea9ffb34ed59
justtaaa <taya.nekrasova003@gmail.com>
2023-11-17 13:11:28 +0000

    #sign up test 1

--- Citisoft/SignUp.cs
+++ Citisoft/SignUp.cs
@@ -1,7 +1,46 @@
-namespase Citisoft
+﻿using System;
+using System.Data.SqlClient;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Citisoft
 {
-    class SignUp
+    public partial class SignUpForm : Form
 {
- 
-}
+    private const string ConnectionString = path;
+
+    public SignUpForm()
+    {
+        InitializeComponent();
+
+    }
+
+    private void SignUpButton_Click(object sender, EventArgs e)
+    {
+        string email = emailTextBox.Text;
+        string password = passwordTextBox.Text;
+
+        //validation 
+
+        if (stringIsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(password))
+        {
+            MessageBox.Show("Please enter bothemail and password.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            return;
+        }
+
+        // add other validation 
+
+        if (SaveCredentialsToDatabase(email, password){
+            MessageBox.Show("Signup is successful!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            this.Close;
+        }
+        else
+        {
+            MessageBox.Show("Error.Please try again later", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+
+
 }
