9c54dde847a247e61d361e538a7a23bea5c3f801
justtaaa <taya.nekrasova003@gmail.com>
2023-11-17 13:33:13 +0000

    # SignUp test2

--- Citisoft/SignUp.cs
+++ Citisoft/SignUp.cs
@@ -8,39 +8,82 @@ using System.Xml.Linq;
 namespace Citisoft
 {
     public partial class SignUpForm : Form
-{
-    private const string ConnectionString = path;
-
-    public SignUpForm()
     {
-        InitializeComponent();
+        private TextBox emailTextBox;
+        private TextBox passwordTextBox;
+        private Button SignUpButton;
 
-    }
 
-    private void SignUpButton_Click(object sender, EventArgs e)
-    {
-        string email = emailTextBox.Text;
-        string password = passwordTextBox.Text;
+        //private const string ConnectionString = path;
 
-        //validation 
-
-        if (stringIsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(password))
+        public SignUpForm()
         {
-            MessageBox.Show("Please enter bothemail and password.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            return;
-        }
+            InitializeComponent();
 
-        // add other validation 
-
-        if (SaveCredentialsToDatabase(email, password){
-            MessageBox.Show("Signup is successful!", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            this.Close;
         }
-        else
+ 
+        private void SignUpButton_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Error.Please try again later", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            string email = emailTextBox.Text;
+            string password = passwordTextBox.Text;
+
+            //validation 
+
+            if (string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(password))
+            {
+                MessageBox.Show("Please enter bothemail and password.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // add other validation 
+
+            if (SaveCredentialsToDatabase(email, password))
+            {
+                MessageBox.Show("Signup is successful!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Close;
+            }
+            else
+            {
+                MessageBox.Show("Error.Please try again later", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private bool SaveCredentialsToDatabase(string email, string password)
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+            }
+        }
+
+        private void InitializeComponent()
+        {
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // EmailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(76, 112);
+            this.emailTextBox.Name = "textBox1";
+            this.emailTextBox.Size = new System.Drawing.Size(143, 20);
+            this.emailTextBox.TabIndex = 0;
+            this.emailTextBox.TextChanged += new System.EventHandler(this.emailTextBox);
+            // 
+            // SignUpForm
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.emailTextBox);
+            this.Name = "SignUpForm";
+            this.Load += new System.EventHandler(this.SignUpForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void SignUpForm_Load(object sender, EventArgs e)
+        {
+
         }
     }
-
-
 }
